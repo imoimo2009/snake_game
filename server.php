d7fc9231151d4e0c807271b10c4c915cbb272c88
imoimo <imoimo@imoimo.xyz>
2022-05-04 23:54:03 +0900

    簡易的な排他制御を追加

--- server.php
+++ server.php
@@ -2,18 +2,21 @@
 
 define("SAVE_FILE","./hiscore.txt");
 
+$result = "NG";
+
 if(isset($_POST["save_hiscore"])) {
-    $hiscore = $_POST["save_hiscore"];
-    file_put_contents(SAVE_FILE,$hiscore);
-    echo "OK";
+    $score = $_POST["save_hiscore"];
+    $hiscore = file_get_contents(SAVE_FILE);
+    if($score > $hiscore){
+        if(file_put_contents(SAVE_FILE,$score,LOCK_EX)){
+            $result = "OK";
+        }
+    }
 }elseif(isset($_GET["load_hiscore"])) {
     if(file_exists(SAVE_FILE)){
         $hiscore = file_get_contents(SAVE_FILE);
-        echo $hiscore;
-    }else{
-        echo "NG";
+        $result = $hiscore;
     }
-}else{
-    echo "NO COMMAND";
 }
+echo $result;
 ?>
